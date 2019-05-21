a2b49af002cce6019ea63233d447f2801984a6f5
Gian Lazzarini <gianlazzarini@gmail.com>
2019-05-20 20:45:48 -0700

    Working on stripe pro payments

--- ionic/src/app/pro-sign-up/pro-sign-up.page.ts
+++ ionic/src/app/pro-sign-up/pro-sign-up.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-pro-sign-up',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProSignUpPage implements OnInit {
 
+  @Input()
+  amount = 300;
+
+  @Input()
+  description;
+
+  handler: StripeCheckoutHandler;
+
+  confirmation: any;
+  loading = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.handler = StripeCheckout.configure({
+      key: '',
+      image: '',
+      locale: 'auto',
+      source: async (source) => {
+        this.loading = true
+        //const user = await this.auth.getUser();
+        //const fun = this.functions.httpsCallable('StripCreateCharge');
+        //this.confirmation = await fun({ source: source.id, uid: user.id, amount: this.amount  }).toPromise();
+        //this.loading = false;
+      }
+    });
   }
+
+  async checkout(e) {
+    //const user = await this.auth.getUser();
+    this.handler.open({
+      name: '',
+      //description: this.description,
+      amount: this.amount,
+      //email: user.email
+    })
+    e.preventDefault();
+  }
+
+  /*
+  @HostListener('window:popstate') {
+    onPopSate() {
+      this.handler.close();
+    }
+  }
+  */
 
 }
