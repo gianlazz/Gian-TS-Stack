9ad4918b3acbd26711d56e2c1d277d2687faa4ed
Gian Lazzarini <gianlazzarini@gmail.com>
2019-07-13 15:35:22 -0700

    Refactored server/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,34 +9,51 @@ import { createDockerDbConnection } from "./deploymentConfigs/createDockerDbConn
 import { createLocalDevDbConnection } from "./deploymentConfigs/createLocalDevDbConnection";
 import { envVariablesConfigured } from "./deploymentConfigs/envChecker";
 import * as graphqlApi from "./graphQL/graphqlApi";
-
 import cookieParser = require("cookie-parser");
+
 console.log("starting server");
+
+// Sets up TypeDI Dependency Injection Container
 useContainer(Container);
 
+// Creating express app
 const app = express();
 // Configure Express to parse incoming JSON data
 app.use( express.json() );
-
+// Used to help parse out cookies from requests
 app.use(cookieParser());
 
-let allowedOrigins = [];
-let corsOptions = {};
+// Configure Express to allow Cross Origin Scripting so server and client can communicate during dev
+let allowedOrigins = [
+        "capacitor://localhost",
+        "ionic://localhost",
+        "http://localhost",
+        "http://localhost:8080",
+        "http://localhost:8100",
+        "http://localhost:8101"
+];
 
-let port = process.env.PORT;
+let corsOptions = {
+        // origin: "http://localhost:8100",
+        origin: true,
+        credentials: true
+};
 
-console.log(port);
+// Register GraphQL setup middleware
+graphqlApi.register( app, corsOptions );
+
 // initialize configuration
 if (process.env.NODE_ENV === "docker") {
 // DEPLOYMENT CONFIGURATION
-    if (!envVariablesConfigured()) {
-        throw new Error(("Missing required environment variables!"));
-    }
 
+    if (!envVariablesConfigured())
+        throw new Error(("Missing required environment variables!"));
+
+    // Typeorm connection    
     console.log("Connecting to docker db.");
     createDockerDbConnection()
-    .then((connection) => console.log("Connected to docker Postgres with TypeORM."))
-    .catch((error) => console.log(error));
+        .then((connection) => console.log("Connected to docker Postgres with TypeORM."))
+        .catch((error) => console.log(error));
 
     // start https server
     const sslOptions = {
@@ -44,70 +61,32 @@ if (process.env.NODE_ENV === "docker") {
         cert: fs.readFileSync("/ssl/cert.pem").toString()
     };
 
-    if (!sslOptions.cert || !sslOptions.key) {
+    if (!sslOptions.cert || !sslOptions.key)
         console.error("SSL files not setup correctly!");
-    } else {
+    else
         console.log(sslOptions);
-    }
-
-    // Configure Express to allow Cross Origin Scripting so server and client can communicate during dev
-    allowedOrigins = [
-        "capacitor://localhost",
-        "ionic://localhost",
-        "http://localhost",
-        "http://localhost:8080",
-        "http://localhost:8100",
-        "http://localhost:8101"
-    ];
-
-    corsOptions = {
-        // origin: "http://localhost:8100",
-        origin: true,
-        credentials: true
-    };
-
-    // Register GraphQL setup middleware
-    graphqlApi.register( app, corsOptions );
 
     const serverHttps = https.createServer(sslOptions, app).listen(443);
+
 } else {
 // DEV CONFIGURATION
-    dotenv.config();
-    // port is now available to the Node.js runtime
-    // as if it were an enviroment variable
-    port = process.env.PORT;
 
-    if (!envVariablesConfigured()) {
+    // Register .env file variables
+    dotenv.config();
+    if (!envVariablesConfigured())
         throw new Error(("Missing required environment variables!"));
-    }
-// Typeorm connection
+    const port = process.env.PORT;
+
+    // Typeorm connection
     console.log("Connecting to local db.");
     createLocalDevDbConnection()
-    .then((connection) => console.log("Connected to default ormconfig.json database with TypeORM."))
-    .catch((error) => console.log(error));
-
-    // Configure Express to allow Cross Origin Scripting so server and client can communicate during dev
-    allowedOrigins = [
-        "capacitor://localhost",
-        "ionic://localhost",
-        "http://localhost",
-        "http://localhost:8080",
-        "http://localhost:8100",
-        "http://localhost:8101"
-    ];
-
-    corsOptions = {
-        // origin: "http://localhost:8100",
-        origin: true,
-        credentials: true
-    };
-
-    // Register GraphQL setup middleware
-    graphqlApi.register( app, corsOptions );
+        .then((connection) => console.log("Connected to default ormconfig.json database with TypeORM."))
+        .catch((error) => console.log(error));
 
     // start the Express server
     app.listen( port, () => {
         console.log( `Server started at http://localhost:${ port }` );
         console.log(`Running a GraphQL API server at http://localhost:${ port }/graphql`);
     } );
+
 }
