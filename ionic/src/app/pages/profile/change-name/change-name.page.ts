0b90395631aadfacceb65c4f2e51ab2bb69ef2e2
Gian Lazzarini <gianlazzarini@gmail.com>
2019-07-17 01:29:40 -0700

    Added reactive form validation and loading bar to change name page.

--- ionic/src/app/pages/profile/change-name/change-name.page.ts
+++ ionic/src/app/pages/profile/change-name/change-name.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ProfileService } from 'src/app/services/profile.service';
 import { AlertService } from 'src/app/services/alert.service';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-name',
@@ -11,25 +11,54 @@ import { NgForm } from '@angular/forms';
 })
 export class ChangeNamePage implements OnInit {
 
+  loading = false;
+
+  myForm: FormGroup;
+
+  get firstName() {
+    return this.myForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.myForm.get('lastName');
+  }
+
   constructor(
     private modalController: ModalController,
     private profileService: ProfileService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.myForm = this.fb.group({
+      firstName: ['', [
+        Validators.required
+      ]],
+      lastName: ['', [
+        Validators.required
+      ]]
+    });
+
+    this.myForm.valueChanges.subscribe();
   }
 
   dismiss() {
     this.modalController.dismiss();
   }
 
-  async changeName(form: NgForm) {
-    const result = await this.profileService.changeName(form.value.firstName, form.value.lastName);
+  async changeName() {
+    this.loading = true;
+
+    const formValue = this.myForm.value;
+
+    const result = await this.profileService.changeName(formValue.firstName, formValue.lastName);
     if (result) {
+      this.loading = false;
       this.alertService.presentToast("Changed name.");
       this.dismiss();
     } else {
+      this.loading = false;
       this.alertService.presentRedToast("Failed to change name.");
     }
   }
