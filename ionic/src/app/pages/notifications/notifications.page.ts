1fc596432f97257ec59fe8e82e3ebcb9bac0f1cc
Gian Lazzarini <gianlazzarini@gmail.com>
2019-08-07 20:55:28 -0700

    Added ionic pull down refresher with loading bar to the notifications page.

--- ionic/src/app/pages/notifications/notifications.page.ts
+++ ionic/src/app/pages/notifications/notifications.page.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsPage implements OnInit {
 
+  loading = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+    this.loading = true;
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+      this.loading = false;
+    }, 2000);
   }
 
 }
